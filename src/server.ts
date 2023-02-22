c700c9dd3cd8f3e1d19832347e68805ea2e909e7
parkjikuk <paxkk@naver.com>
2023-02-22 23:37:39 +0900

    add socket.io database server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
+import { ChatMessage, ChatMessageModel } from './models/ChatMessageModel';
 import express from "express";
 import cors from 'cors';
 import userRoutes from "./routes/UserRoutes";
+import { Server, Socket } from 'socket.io';
+
 
 const mongoose = require('mongoose');
 
@@ -12,16 +15,52 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/beerApp')
-.then(() => {
-  console.log("연결 성공!!!!");
-})
-.catch((err: any) => {
-  console.log(err.message);
+const beerAppDb = mongoose.createConnection('mongodb://127.0.0.1:27017/beerApp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const chatDb = mongoose.createConnection('mongodb://127.0.0.1:27017/chat', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+beerAppDb.once("open", () => {
+  console.log("beerApp 데이터베이스 연결")
+});
+
+chatDb.once("open", () => {
+  console.log("chat 데이터베이스 연결 성공")
 });
 
 app.use("/api/user", userRoutes);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log("server started on port 5000")
+});
+const io = new Server(server);
+
+io.on("connection", (socket: Socket) => {
+  console.log(`소켓 연결 ${socket.id}`)
+
+  socket.on("join room", (roomId: string) => {
+    console.log(`소켓 ${socket.id} joined room ${roomId}`);
+    socket.join(roomId);
+  })
+
+  socket.on("send message", (data: ChatMessage) => {
+    const chatMessage = new ChatMessageModel(data);
+    chatMessage.save();
+
+    io.to(data.roomId).emit('receive message', data);
+  });
+
+  socket.on("leave room", (roomId: string) => {
+    console.log(`소켓 ${socket.id} left room ${roomId}`);
+    socket.leave(roomId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`소켓 연결 끊김 : ${socket.id}`);
+  });
 });
