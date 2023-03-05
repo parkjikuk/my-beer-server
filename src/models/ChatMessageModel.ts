54be6e91ebb5e83c97b2726f06672e7634858efc
parkjikuk <paxkk@naver.com>
2023-03-05 23:59:48 +0900

    add ChatMessage Schme property

--- src/models/ChatMessageModel.ts
+++ src/models/ChatMessageModel.ts
@@ -1,15 +1,24 @@
 import mongoose from 'mongoose';
 
 export interface ChatMessage {
-  id: string;
+  _id: string;
   message: string;
   roomId: string;
+  userName: string;
+  email: string;
+  myMessage: boolean;
 }
 
 const ChatMessageSchema = new mongoose.Schema({
-  id: String,
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    auto: true,
+  },
   message: String,
   roomId: String,
+  userName: String,
+  email: String,
+  myMessage: Boolean,
 });
 
 export const ChatMessageModel = mongoose.model<ChatMessage>("ChatMessage", ChatMessageSchema)
