ce7738cbae522eec88ebc0c5d353a79dfae5edb9
parkjikuk <paxkk@naver.com>
2023-03-06 00:01:07 +0900

    add request body email and myMessage

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -4,7 +4,9 @@ import { ChatMessageModel } from "../models/ChatMessageModel";
 export const getChatMessages = async (req: Request, res: Response) => {
   const { roomId } = req.params;
   try {
-    const messages = await ChatMessageModel.find({ roomId });
+    const messages = await ChatMessageModel.find({ roomId })
+    .sort({ _id: -1 })
+    .limit(10)
     res.status(200).json(messages);
   } catch (error) {
     console.error(error);
@@ -14,13 +16,12 @@ export const getChatMessages = async (req: Request, res: Response) => {
 
 export const postChatMessage = async (req: Request, res: Response) => {
   const { roomId } = req.params;
-  const { username, content } = req.body;
-  const message = new ChatMessageModel({ roomId, username, content });
+  const { userName, message, email, myMessage} = req.body;
+  const chatMessage  = new ChatMessageModel({ roomId, userName, message, email, myMessage });
   try {
-    await message.save();
-    res.status(201).json(message);
+    res.status(201).json(chatMessage);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "서버 에러" });
+    res.status(500).json({ message : "서버 에러" });
   }
 };
