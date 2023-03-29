5a59fa46eaf86bdbcc756bfbc0ed09be61c74371
parkjikuk <paxkk@naver.com>
2023-03-30 00:33:35 +0900

    Fix CORS policy error

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,8 @@ import { Server, Socket } from 'socket.io';
 import chatRoutes from "./routes/ChatRoutes";
 require('dotenv').config();
 
-const dbUri = process.env.MONGODB_URI;
-const clientUrl = process.env.CLIENT_URL;
+const dbUri: string = process.env.MONGODB_URI || '' ;
+const clientUrl: string = process.env.CLIENT_URL || '' ;
 
 const mongoose = require('mongoose');
 
